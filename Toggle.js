9c5823768ab15fa98c2343c97b52998942c3bec6
SethTML <123321772+SethTML@users.noreply.github.com>
2023-05-28 10:16:19 -0400

    Update Toggle.js

--- Toggle.js
+++ Toggle.js
@@ -1,2 +1,5 @@
-let toggleName = ("") // Enter Selected Toggle into here
+let toggleName = window.alert("Create Toggle Id") // Enter Selected Toggle into here
 localStorage.setItem(toggleName,true);
+
+
+// F12 then select Console. Enter above code into console.
